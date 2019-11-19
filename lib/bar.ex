906245b0087b6e151dd06dad1c06ad7c100dbf48
André Betiolo <andrebetiolo@gmail.com>
2019-11-19 15:22:13 -0400

    Test

---
+++ lib/bar.ex
@@ -0,0 +1,18 @@
+defmodule Bar do
+  @moduledoc """
+  Documentation for Bar.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bar.hello()
+      :world
+
+  """
+  def say do
+    "bar"
+  end
+end
